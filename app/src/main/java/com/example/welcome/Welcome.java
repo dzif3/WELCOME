5e3761ba52641d80e4957803b409df03f03c9ce3
dzikra <dzikrahappy@gmail.com>
2021-11-10 00:50:43 +0700

    Welcome

---
+++ app/src/main/java/com/example/welcome/Welcome.java
@@ -0,0 +1,28 @@
+package com.example.welcome;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Welcome extends AppCompatActivity {
+
+    Button btnExit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnExit = (Button) findViewById(R.id.btnLogin);
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //perintah untuk mengakhiri aplikasi
+                finish();
+            }
+        });
+
+    }
+}
